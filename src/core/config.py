763c13ff48385c3e28bba08231ed0fba1b81932a
athropyne <anarchia.rulit@gmail.com>
2025-07-24 09:27:11 +0300

    + config file

---
+++ src/core/config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='../.env', env_file_encoding='utf-8')
+
+    # server
+    SERVER_HOST: str = "localhost"
+    SERVER_PORT: int = 10000
+
+
+
+settings = Settings()
