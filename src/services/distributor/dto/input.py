c9b7567bc5d9223e83767c0c26ead9988b626727
athropyne <anarchia.rulit@gmail.com>
2025-08-01 16:31:20 +0300

    fix: POST '/distributor'. Updated user_id type str to int

--- src/services/distributor/dto/input.py
+++ src/services/distributor/dto/input.py
@@ -5,5 +5,5 @@ class INPUT_NewBotParams(BaseModel):
     bot_token: str = Field(..., description="Your bot token")
     bot_title: str = Field(..., description="Your bot title")
     bot_username: str = Field(..., description="Your bot username")
-    creator_id: str = Field(..., description="Creator user id")
+    creator_id: int = Field(..., description="Creator user id")
     bot_link: str = Field(..., description="Bot lint")
